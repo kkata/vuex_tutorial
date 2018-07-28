9c39a3979ff4059d96a8d9f1a520b70ecfe70998
kkata <redprin900@yahoo.co.jp>
2018-07-28 18:06:13 +0900

    wip

---
+++ src/components/ProductList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <img
+      v-if="loading"
+      src="https://i.imgur.com/JfPpwOA.gif"
+      alt=""
+    >
+    <ul v-else>
+      <li v-for="product in products" :key="product.id">{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import store from '@/store/index'
+
+export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+  computed: {
+    products () {
+      return store.getters.availableProducts
+    }
+  },
+  created() {
+    this.loading = true
+    setTimeout(() => {
+      store.dispatch('fetchProducts')
+        .then((() => this.loading = false))
+    }, 500);
+
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    products: []
+  },
+
+  getters: { // computed properties
+      availableProducts (state, getters) {
+        return state.products.filter(product => product.inventory > 0)
+      }
+  },
+
+  actions: { // = methods
+    fetchProducts ({commit}) {
+      return new Promise((resolve, reject) => {
+        // make the call
+        // run setProducts mutation
+        fetch('http://localhost:3000/products')
+          .then( res => res.json() )
+          // .then( res => store.state.products = res )
+          .then( res => {
+            commit('setProducts', res )
+            resolve()
+          })
+      })
+    }
+  },
+
+  mutations: {
+    setProducts (state, products) {
+      // update products
+      state.products = products
+    }
+  }
+})
